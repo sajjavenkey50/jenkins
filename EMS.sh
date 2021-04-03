f5afe1f4defd858da7a45c09319fbbc0d969f504
sajjavenkey50 <48043757+sajjavenkey50@users.noreply.github.com>
2021-04-03 10:04:03 +0530

    Update EMS.sh

--- EMS.sh
+++ EMS.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-cd TIB_BW_5.13.0_win_x86_64
+cd TIB_BW_6.5.1_linux26gl23_x86_64
 ./TIBCOUniversalInstaller-lnx-x86-64.bin -silent -V responseFile="TIBCOUniversalInstaller-ems.silent"
